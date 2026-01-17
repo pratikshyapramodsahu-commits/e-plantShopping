df22151222c2ab27bace53e7a2a2fe2f0ed0714e
Pratikshya Sahu <pratikshyapramodsahu@gmail.com>
2026-01-17 21:25:42 +0530

    main file updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {HashRouter} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   <Provider store={store}>
-  <HashRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+  
     <App />
-  </HashRouter>
     
-  //   </Provider>
-  // </React.StrictMode>,
+     </Provider>
+   </React.StrictMode>,
 );
